05bac3a26f2eaa100738a61e47d779917d893db0
Arthur Monteiro <monteiroamelo@gmail.com>
2024-06-06 21:11:17 -0300

    grounded

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'components/game.dart'; // Add this import
+
 void main() {
   runApp(
-    GameWidget.controlled(
-      gameFactory: FlameGame.new,
+    const GameWidget.controlled(
+      gameFactory: MyPhysicsGame.new, // Modify this line
     ),
   );
 }
